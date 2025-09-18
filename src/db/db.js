05d563c66dafe0a8efc20676f4065daf5be1280e
AdnanAmin045 <adnanamin.available@gmail.com>
2025-09-19 01:29:27 +0500

    fix signUp and signIn

--- src/db/db.js
+++ src/db/db.js
@@ -5,7 +5,7 @@ import { initializeServices } from "../utils/InitDb.js";
 const connectDB=async()=>{
     try {
         const connectioninstance=await mongoose.connect(`${process.env.MONGODB_URI}`)
-      //  await mongoose.connection.dropDatabase();
+     // await mongoose.connection.dropDatabase();
         await initializeServices();
     console.log(`\n mongodb connected successfully`);
     } catch (error) {
